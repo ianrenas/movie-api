ba760d06ad2699a52c4a48408aa4ea8b335132d9
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-06-29 21:58:49 -0400

    Update scripts.js
    fixed spacing in code

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,6 @@ for (let i = 0; i < pokemonList.length; i++)
   document.write (pokemonList[i].name + "(height: " + pokemonList[i].height + ")");
 }
   //if pokemon height is greater than 1.8, writes Wow, that's big
-if (pokemonList[i]. height > 1.8){
-  document.write (" -Wow, that's big!");
+if (pokemonList[i].height > 1.8) {
+  document.write(" -Wow, that's big!");
 }
