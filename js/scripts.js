b2ea4c9a6a5dcbdbd300e756888844b8d789a66b
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-07-19 23:43:04 -0400

    Update scripts.js
    Updated code for better workability

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,64 @@
-let pokemonList = [
-  {
-    name: 'Venusaur',
-    height: 2,
-    types: ['grass', 'poison'],
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire', 'flying'],
-  },
-  {
-    name: 'Nidoking',
-    height: 1.4,
-    types: ['ground', 'poison'],
-  },
-];
-
 let pokemonRepository = (function() {
+	let pokemonList = [
+		{
+			name: "Venusaur",
+			height: 2,
+			types: ["grass", "poison"],
+		},
+		{
+			name: "Charizard",
+			height: 1.7,
+			types: ["fire", "flying"],
+		},
+		{
+			name: "Nidoking",
+			height: 1.4,
+			types: ["ground", "poison"],
+		},
+	];
 
-   let pokemonList = [];
+	function getAll() {
+		return pokemonList;
+	}
 
-   function add(pokemon) {
-      if (typeof pokemon === "object") {
-         pokemonList.push(pokemon);
-      }
-  }
+	function add(pokemon) {
+		if (typeof pokemon === "object" && "name" in pokemon) {
+			pokemonList.push(pokemon);
+		} else {
+			console.log(
+				"pokemon needs to be an object and contains a name property"
+			);
+		}
+	}
 
-   function getAll() {
-      return pokemonList;
-   }
+	return {
+		getAll: getAll,
+		add: add
+	}
 
-   return {
-      add: add,
-      getAll: getAll
-   }
 })();
 
-pokemonRepository.add({
-    name: 'Venusaur',
-    height: 2,
-    types: ['grass', 'poison']
-});
+let newPokemon = {
+  name: "Charmander",
+  height: 0.6,
+  types: ["fire"]
+};
+
+pokemonRepository.add(newPokemon);
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write (pokemon.name + pokemon.height);
-});
-
+  //Pokemon name and height to the DOM
+  document.write(pokemon.name + "(height: " + pokemon.height + ")");
 
   //if pokemon height is greater than 1.8, writes Wow, that's big
   if (pokemon.height > 1.7) {
     document.write (" -Wow, that's big!");
-  }
+	}
+
+	//Adds line breaks after each pokemon name
+  document.write("<br><br>");
+});
+
+
+
+// TO HERE
