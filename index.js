90baa7488bdff76b7dc9e51cfa449fdd216fa898
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-08-24 22:52:16 -0400

    Update index.js
    added requirements for task 2.4

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express'),
-  morgan = require('morgan');
+morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan('common'));
+// Get Request-Returns my top ten movies
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
+});
 
+//Get Request-
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+// Get Request-Returns a welcome message
 app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
+  res.send('Welcome to my Super-80s-Flix movie database!');
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+//Logging Middleware
+app.use(morgan('common'));
+// For sending static files
+app.use(express.static('public'));
+
+//Error Handling Middlware
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send('Oops! Something has gone wrong');
 });
+
+
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
