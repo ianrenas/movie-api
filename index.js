d487a34d0df736eececf6231053777c64cd3c053
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-08-27 23:40:54 -0400

    Update index.js
    fixed errors in code

--- index.js
+++ index.js
@@ -81,87 +81,63 @@ app.get('/movies', (req, res) => {
 
 // Get data about a certain movie, by title
 app.get('/movies/:Title', (req, res) => {
-  res.json(topTenMovies.find((movie) => {
-    return movie.title === req.params.title
-  }));
+  res.send("Successful GET request returning data about a certain movie")
 });
 
 // Get data about genre
 app.get('/genres/:Name', (req, res) => {
-  res.json(genres);
+  res.send("Successful GET request returning genres")
 });
 
 // Get data about director
-app.get('/directors/:Name', (req, res) => {
-  res.json(directors);
+app.get("/directors/:Name", (req, res) => {
+  res.send("Successful GET request returning directors");
 });
 
-// Add a new user
-app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.title) {
-    const message = 'Missing username in request body';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    topTenMovies.push(newUser);
-    res.status(201).send(newUser);
-  }
+// Add a new user to register
+app.post("/users", (req, res) => {
+  res.send("Successful POST adding user");
 });
 
 // Update user info
+app.put("/users/:username", (req, res) => {
+  res.send("Successful PUT updating user info ");
+});
 
 
 // Add a movie
 app.post('/users/:Username/favorites/:MovieID', (req, res) => {
-  let newMovie = req.body;
-
-  if (!newMovie.title) {
-    const message = 'Missing movie title in request body';
-    res.status(400).send(message);
-  } else {
-    newMovie.id = uuid.v4();
-    topTenMovies.push(newMovie);
-    res.status(201).send(newMovie);
-  }
+  res.send("Successful POST adding movie to favorites");
 });
 
 // Delete a movie from the list of favorites
 app.delete('/users/:Username/removeFromFav/:MovieID', (req, res) => {
-  let movie = topTenMovies.find((movie) => {
-    return movie.id === req.params.id
+  res.send("Successful DELETE of movie from list of favorites");
+});
+
+  // Allows users to deregister
+  app.delete('/users', (req, res) => {
+    res.send("Successful DELETE of user");
   });
 
-  if (movie) {
-    topTenMovies = topTenMovies.filter((obj) => { return obj.id !== req.params.id });
-    res.status(201).send('Movie with the ID of ' + req.params.id + ' was deleted.');
-  } else {
-    res.status(404).send(`Movie with the id ${req.params.id} was not found.`);
-  }
-});
-
-// Allows users to deregister
 
 
 
-
-
-//Get Request-
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
-
-
-
-//Error Handling Middlware
-app.use((err, req, res, next) => {
-  console.log(err.stack);
-  res.status(500).send('Oops! Something has gone wrong');
-});
+  //Get Request-
+  app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
 
 
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
-});
+  //Error Handling Middlware
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send('Oops! Something has gone wrong');
+  });
+
+
+
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
