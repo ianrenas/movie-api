8dcb094e4eaaed8485e201c8d62abc48de5fe43c
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-09-13 22:41:50 -0400

    Update models.js
    updated per requirements

--- models.js
+++ models.js
@@ -24,8 +24,11 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
+
+
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+
 
 module.exports.Movie = Movie;
 module.exports.User = User;
